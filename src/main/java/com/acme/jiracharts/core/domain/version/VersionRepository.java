8966af9db8ccdff7c483bb2900c9eb1bbaadd514
daniele.canteri <daniele.canteri@gmail.com>
2013-09-30 23:19:16 +0200

    project filters gadget

--- src/main/java/com/acme/jiracharts/core/domain/version/VersionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.acme.jiracharts.core.domain.version;
-
-import com.atlassian.jira.project.Project;
-
-public interface VersionRepository {
-
-	Version firstUnreleasedOfProject(Project project);
-
-}
