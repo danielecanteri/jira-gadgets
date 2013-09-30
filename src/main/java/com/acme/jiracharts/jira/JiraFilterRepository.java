32429105fb8c58ba83f1824affeccb2bfc4b2ae7
daniele.canteri <daniele.canteri@gmail.com>
2013-09-30 23:19:56 +0200

    project filters gadget

--- src/main/java/com/acme/jiracharts/jira/JiraFilterRepository.java
+++ src/main/java/com/acme/jiracharts/jira/JiraFilterRepository.java
@@ -1,36 +1,40 @@
 package com.acme.jiracharts.jira;
 
+import java.util.ArrayList;
 import java.util.List;
 
-import com.acme.jiracharts.core.domain.issue.IssueRepository;
-import com.acme.jiracharts.core.domain.version.Version;
+import com.acme.jiracharts.core.domain.filter.Filter;
+import com.acme.jiracharts.core.domain.filter.FilterRepository;
 import com.atlassian.crowd.embedded.api.User;
-import com.atlassian.jira.bc.issue.search.SearchService;
-import com.atlassian.jira.issue.Issue;
-import com.atlassian.jira.issue.search.SearchException;
-import com.atlassian.jira.issue.search.SearchResults;
-import com.atlassian.jira.jql.builder.JqlQueryBuilder;
-import com.atlassian.jira.web.bean.PagerFilter;
-import com.atlassian.query.Query;
+import com.atlassian.jira.bc.JiraServiceContextImpl;
+import com.atlassian.jira.bc.filter.SearchRequestService;
+import com.atlassian.jira.issue.search.SearchRequest;
+import com.atlassian.jira.sharing.search.ProjectShareTypeSearchParameter;
+import com.atlassian.jira.sharing.search.SharedEntitySearchParametersBuilder;
+import com.atlassian.jira.sharing.search.SharedEntitySearchResult;
 
-public class JiraIssueRepository implements IssueRepository {
+public class JiraFilterRepository implements FilterRepository {
 
-	private SearchService searchService;
+	private SearchRequestService searchRequestService;
 
-	public JiraIssueRepository(SearchService searchService) {
-		this.searchService = searchService;
+	public JiraFilterRepository(SearchRequestService searchRequestService) {
+		this.searchRequestService = searchRequestService;
 	}
 
 	@Override
-	public List<Issue> allIssuesForVersion(User user, Version firstUnreleased) {
-		try {
-			Query query = JqlQueryBuilder.newBuilder().where()
-					.fixVersion(firstUnreleased.getId()).buildQuery();
-			SearchResults search = searchService.search(user, query,
-					PagerFilter.getUnlimitedFilter());
-			return search.getIssues();
-		} catch (SearchException e) {
-			throw new RuntimeException(e);
+	public List<Filter> filtersSharedByProject(User user, Long projectId) {
+		SharedEntitySearchParametersBuilder b = new SharedEntitySearchParametersBuilder();
+		b.setShareTypeParameter(new ProjectShareTypeSearchParameter(projectId));
+		SharedEntitySearchResult<SearchRequest> search = searchRequestService
+				.search(new JiraServiceContextImpl(user),
+						b.toSearchParameters(), 0, 100);
+		List<Filter> result = new ArrayList<Filter>();
+		for (SearchRequest searchRequest : search.getResults()) {
+			Filter filter = new Filter();
+			filter.setId(searchRequest.getId());
+			filter.setName(searchRequest.getName());
+			result.add(filter);
 		}
+		return result;
 	}
 }
